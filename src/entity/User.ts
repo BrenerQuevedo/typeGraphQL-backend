5507738fae21e3ed1e7edba4c2b2852fe42229e2
BrenerQuevedo <39017457+BrenerQuevedo@users.noreply.github.com>
2020-03-04 04:29:13 -0300

    create user table

---
+++ src/entity/User.ts
@@ -0,0 +1,21 @@
+import {Entity, PrimaryGeneratedColumn, Column, BaseEntity} from "typeorm";
+
+@Entity()
+export class User extends BaseEntity{
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column("text", {unique:true })
+    email: string;
+
+    @Column()
+    password: string;
+
+}
