0035c07c938818cd4aa28c5af4464d94ab6a9a6c
BrenerQuevedo <39017457+BrenerQuevedo@users.noreply.github.com>
2020-03-04 06:23:24 -0300

    refactoring user model

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,19 +1,30 @@
 import {Entity, PrimaryGeneratedColumn, Column, BaseEntity} from "typeorm";
+import { ObjectType, Field, ID } from "type-graphql";
 
+
+//pode ser passado como retorno de uma mutation agr
+@ObjectType()
 @Entity()
 export class User extends BaseEntity{
-
+    @Field(() => ID)
     @PrimaryGeneratedColumn()
     id: number;
 
+    @Field()
     @Column()
     firstName: string;
 
+    @Field()
     @Column()
     lastName: string;
 
+    @Field()
     @Column("text", {unique:true })
     email: string;
+
+    @Field()
+    name: string;   
+
 
     @Column()
     password: string;
