3d55fb20f502db5d76526a893a145cf43b9b1dcd
BrenerQuevedo <39017457+BrenerQuevedo@users.noreply.github.com>
2020-03-07 02:57:13 -0300

    config redis, cors and starting logging

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import "reflect-metadata"
 
 import { ApolloServer } from "apollo-server-express";
-import  Express from "express";
+import Express from "express";
 import { buildSchema } from "type-graphql";
-import {createConnection} from "typeorm";
+import { createConnection } from "typeorm";
 import { RegisterResolver } from "./modules/user/register";
+import session from "express-session"
+import connectRedis from "connect-redis"
+import cors from "cors";
 
+
+import { redis } from "./redis"
 
 const main = async () => {
 
@@ -16,7 +21,7 @@ const main = async () => {
         username: "test",
         password: "test",
         database: "typegraphql",
-        entities:["src/entity/*.*"],
+        entities: ["src/entity/*.*"],
         synchronize: true,
         logging: true,
     });
@@ -26,9 +31,35 @@ const main = async () => {
         resolvers: [RegisterResolver],
     });
 
-    const apolloServer = new ApolloServer({ schema });
+    const apolloServer = new ApolloServer({
+        schema,
+        //acesso a key context para o acesso dos Resolvers, Apollo Server dá acesso ao req obj, garantindo acesso ao dados da session
+        context: ({ req }: any) => ({ req })
+    });
 
     const app = Express();
+
+    app.use(cors())
+
+    const RedisStore = connectRedis(session);
+
+
+    const sessionOption: session.SessionOptions = {
+        store: new RedisStore({
+            client: (redis as any),
+        }),
+        name: "qlid",
+        secret: "testeteste",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 1000 * 60 * 60 * 24 * 7 * 365,
+        },
+    };
+
+    app.use(session(sessionOption));
 
     apolloServer.applyMiddleware({ app })
 
