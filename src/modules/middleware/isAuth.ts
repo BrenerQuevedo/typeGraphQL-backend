2b0ace8e09073a03ad64cff118d3a13ecc3efef3
BrenerQuevedo <39017457+BrenerQuevedo@users.noreply.github.com>
2020-03-11 01:12:26 -0300

    adding middleware

---
+++ src/modules/middleware/isAuth.ts
@@ -0,0 +1,10 @@
+import {MiddlewareFn} from "type-graphql";
+import { MyContext } from "../../types/MyContext";
+
+export const isAuth: MiddlewareFn<MyContext> = async ({context}, next) => {
+    if(!context.req.session!.userId) {
+        throw new Error("User not authorized")
+    }
+
+    return next();
+};
