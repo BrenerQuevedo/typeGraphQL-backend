9899eb7823c8b99482ce0ca4f8e24fa9325d94fc
BrenerQuevedo <39017457+BrenerQuevedo@users.noreply.github.com>
2020-03-12 21:23:46 -0300

    fixing middlewares and Fetching user

--- src/modules/middleware/logger.ts
+++ src/modules/middleware/logger.ts
@@ -1,5 +1,5 @@
 import {MiddlewareFn} from "type-graphql";
-import { MyContext } from "../../types/MyContext";
+import { MyContext } from "../types/MyContext";
 
 export const logger: MiddlewareFn<MyContext> = async ({args}, next) => {
     console.log("arguments:",args);
