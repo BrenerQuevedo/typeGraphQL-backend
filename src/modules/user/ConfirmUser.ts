6052fe569a782e41d8d32e20e1d2d7bebe0775a9
BrenerQuevedo <39017457+BrenerQuevedo@users.noreply.github.com>
2020-03-14 01:47:23 -0300

    finish confirmation

--- src/modules/user/ConfirmUser.ts
+++ src/modules/user/ConfirmUser.ts
@@ -1,31 +1,27 @@
 import {
     Resolver,
     Mutation,
-    Arg,
-    Ctx
+    Arg
 } from "type-graphql";
 
-import { MyContext } from "../../types/MyContext";
 import { redis } from "../../redis";
 import { User } from "../../entity/User";
 
- 
+
 @Resolver()
 export class ConfirmUserResolver {
     @Mutation(() => Boolean)
     async confirmUser(
-        @Arg("token") token: string,
-        @Ctx() context: MyContext
-    ): Promise<Boolean> {
+        @Arg("token") token: string,): Promise<Boolean> {
+
         const userId = await redis.get(token)
 
-
-        if(!userId) {
+        if (!userId) {
             return false;
         }
 
         //update do estado de confirmação do email
-        await User.update({id: parseInt(userId, 10)}, {confirmed: true});
+        await User.update({ id: parseInt(userId, 10) }, { confirmed: true });
         //deleta o token após a confirmação
         await redis.del(token);
 
