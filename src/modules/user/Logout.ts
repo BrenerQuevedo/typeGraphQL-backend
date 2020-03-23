8a4715a358dda33f8bc6aa5e737dbadc44537d8a
BrenerQuevedo <39017457+BrenerQuevedo@users.noreply.github.com>
2020-03-22 22:59:48 -0300

    logout

--- src/modules/user/Logout.ts
+++ src/modules/user/Logout.ts
@@ -1,14 +1,18 @@
-import {Resolver, Mutation, Ctx} from "type-graphql"
+import { Resolver, Mutation, Ctx } from "type-graphql"
 import { MyContext } from "../../types/MyContext";
 
 @Resolver()
 export class LogoutResolver {
-    @Mutation()
-    async logout(@Ctx() context:MyContext) {
-         context.req.session!.destroy(err => {
-             if (err) {
-                console.log(err)
-             }
-         });
+    @Mutation(() => Boolean)
+    async logout(@Ctx() context: MyContext): Promise<Boolean> {
+        return new Promise(
+            (res, rej) => context.req.session!.destroy(err => {
+                if (err) {
+                    console.log(err);
+                    rej(false);
+                }
+                res(true);
+            })
+            );
     }
 }
